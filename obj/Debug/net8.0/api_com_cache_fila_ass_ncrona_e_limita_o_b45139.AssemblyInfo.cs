511e2025f0b646088c4abfa2477b2ead6d237b8d
BrunoDyas <brunodyas@users.noreply.github.com>
2026-04-27 11:53:01 -0300

    feat: configuração estática
    intent: feature

--- obj/Debug/net8.0/api_com_cache_fila_ass_ncrona_e_limita_o_b45139.AssemblyInfo.cs
+++ obj/Debug/net8.0/api_com_cache_fila_ass_ncrona_e_limita_o_b45139.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api_com_cache_fila_ass_ncrona_e_limita_o_b45139")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+893292c8a7ca791da8e699903582fb4ce57002fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49ec08c2891a32582e06148d9fa1ce4d34b09cc6")]
 [assembly: System.Reflection.AssemblyProductAttribute("api_com_cache_fila_ass_ncrona_e_limita_o_b45139")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api_com_cache_fila_ass_ncrona_e_limita_o_b45139")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
